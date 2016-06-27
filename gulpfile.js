f89292e561ecb5c09d96ee10d68c7b38200cc9be
Ranjan Datta <ranjan@adoptapet.com>
2016-06-27 11:28:33 -0700

    gulp tasks split into files

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,17 @@
 var gulp = require('gulp'),
-    sassdoc = require('sassdoc'),
-    doctheme = require('sassdoc-theme-flippant'),
     plugin = require('gulp-load-plugins')({ //this loads all plugins so no need to reload them in individual scripts
         rename: {
-
+            'flippant': 'sassdoc-theme-flippant'
         }
     });
 
 gulp.task('compile:css', require('./gulp/scss')(gulp, plugin));
+gulp.task('generate:docs', require('./gulp/docs')(gulp, plugin));
 
 gulp.task('watch', function () {
     'use strict';
 
-    var scss = gulp.watch('./src/**/*', ['compile:css']),
+    var scss = gulp.watch('./src/**/*', ['compile:css', 'generate:docs']),
         changed = function (event) {
             console.log('File ' + event.path + ' was ' + event.type + '. Running tasks...');
         };
@@ -21,25 +20,4 @@ gulp.task('watch', function () {
 
 });
 
-gulp.task('sassdoc', function () {
-    var options = {
-        dest: 'docs',
-        verbose: true,
-        theme: doctheme,
-        display: {
-            access: ['public', 'private'],
-            alias: true,
-            watermark: true,
-        },
-        groups: {
-            'undefined': 'Bourbon',
-            flexed: 'Flexed'
-        },
-        basePath: 'https://github.com/SassDoc/sassdoc', // our git repo
-    };
-
-    return gulp.src(['./src/bourbon5/**/*.scss','./src/_flexed.scss'])
-        .pipe(sassdoc(options));
-});
-
-gulp.task('default', ['compile:css', 'watch']);
+gulp.task('default', ['compile:css', 'generate:docs', 'watch']);
